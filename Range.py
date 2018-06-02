a1b5089bb47369c3fd2602b629da35583d4b1042
Gaurav Padawe <39410780+GauravPadawe@users.noreply.github.com>
2018-06-02 20:09:31 +0530

    Update Range.py

--- Range.py
+++ Range.py
@@ -1,7 +1,9 @@
-﻿def set_range(a, b, c):
-    x = max (a, b, c)
-    y = min (a, b, c)
-    z = x - y
-    return z
+def set_range(a, b, c):            # Defining Procedure which accepts 3 numerical inputs
+    x = max (a, b, c)              # Variable X will initialise "max()" ,i.e, built-in function - looks for Greater value in (a, b, c)
+    y = min (a, b, c)              # Variable Y will initialise "min()" ,i.e, built-in function - looks for Smaller value in (a, b, c)
+    z = x - y                      # Variable Z will store value of Greater value from X minus Smaller value from Y
+    return z                       # Return Z
     
-print set_range(10,4,7)
+print set_range(10,4,7)            # Print set_range by passing value as per your Preference
+
+# CODED BY - GAURAV PADAWE
